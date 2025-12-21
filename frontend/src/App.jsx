6a9008a0910a111208ea9a78344ddb9f7236a799
kharelranjan572-boop <kharelranjan572@gmail.com>
2025-12-21 11:25:21 +0545

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <div className='p-9 text-slate-900'>This is vite , react , tailwindcss </div>
+    </>
+  )
+}
+
+export default App
